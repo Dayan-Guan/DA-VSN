ed0fdee05df3ab143839b784043f4508c395c1d2
Dayan-Guan <75825029+Dayan-Guan@users.noreply.github.com>
2021-07-26 16:58:49 +0800

    Delete davsn directory

--- davsn/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-
-project_root = pathlib.Path(__file__).resolve().parents[2]
-project_root_ADVENT = pathlib.Path(__file__).resolve().parents[3] / 'ADVENT'
-
-__all__ = ['project_root', 'project_root_ADVENT']
